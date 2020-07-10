d702f055a50200025c339b44e1c0d942194eb843
Vladimir-Zaitsev <gaara2113@rambler.ru>
2020-07-10 19:01:18 +0400

    проект

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -92,126 +92,106 @@ let mySwiper = new Swiper ('.swiper-container', {
   }
 });
 
-(function scrollTop () {
-  let buttonTop = document.querySelector(".button-arrow_js");  
-
-  window.addEventListener("scroll", function(event) {
-    if (window.pageYOffset > 1500) {
-      buttonTop.classList.remove("hidden");
-      buttonTop.addEventListener("click", function handler(event) {
-        buttonTop.classList.add("hidden");
-        window.scrollTo({top: 0, behavior: "auto"});
-      })
-    }
-    if (window.pageYOffset < 1500) {
-      buttonTop.classList.add("hidden");
-    }
-  });
-})();
-
-
-function getValuesForm(form) {
-  //отдаем значения input
-  let body = {};
-  const inputs = form.querySelectorAll("input");
-  const textarea = form.querySelectorAll("textarea");
-  let l = inputs.length;
-  for(let i = 0; i < l; i++) {
-    const input = inputs[i];
-    //проверка типа input
-    switch (input.type) {
-      case "radio":
-        if (input.checked) {
+  function getValuesForm(form) {
+    //отдаем значения input
+    let body = {};
+    const inputs = form.querySelectorAll("input");
+    const textarea = form.querySelectorAll("textarea");
+    let l = inputs.length;
+    for(let i = 0; i < l; i++) {
+      const input = inputs[i];
+      //проверка типа input
+      switch (input.type) {
+        case "radio":
+          if (input.checked) {
+            body[input.name] = input.value;
+          }
+          break;
+        case "checkbox":
+          if(!body[input.name]){
+            body[input.name] = [];
+          }
+          if(input.checked){
+            const inputL = body[input.name].length;
+            body[input.name][inputL] = input.value;          
+          }
+          break;
+        case "file":
+          body[input.name] = input.files;
+          break;
+        default:
           body[input.name] = input.value;
-        }
-        break;
-      case "checkbox":
-        if(!body[input.name]){
-          body[input.name] = [];
-        }
-        if(input.checked){
-          const inputL = body[input.name].length;
-          body[input.name][inputL] = input.value;          
-        }
-        break;
-      case "file":
-        body[input.name] = input.files;
-        break;
-      default:
-        body[input.name] = input.value;
-        break;
+          break;
+      }
     }
+  
+    // let l = textarea.length;
+    // for (let i = 0; i < l; i++) {
+    //   const input = inputs[i];
+    //   body[input.name] = input.value;
+    // }
+    return body;
   }
-
-  // let l = textarea.length;
-  // for (let i = 0; i < l; i++) {
-  //   const input = inputs[i];
-  //   body[input.name] = input.value;
-  // }
-  return body;
-}
-
-function mailCheck (email) {
-  return email.match(/^[0-9a-z-\.]+\@[0-9a-z-]{2,}\.[a-z]{2,}$/i);
-}
-
-function errorMessageInputCreate(input, text) {
-  let message = document.createElement("div");
-  message.classList.add("invalid-feedback");
-  message.innerText = text;
-
-  input.insertAdjacentElement("afterend", message);
-  input.addEventListener("input", function hendlerInput(event) {
-    message.remove();
-    input.removeEventListener("input", hendlerInput);
-  })
-}
-
-function setInvalidInput (input) {
-  input.classList.add("form-input_notvalid");
-  input.addEventListener("input", function hendlerInput(event) {
-    input.classList.remove("form-input_notvalid");
-    input.removeEventListener("input", hendlerInput);
-  })
-}
-
-function setFormErrors (form, errors) {
-  const inputs = form.querySelectorAll("input");
-  const textares = form.querySelectorAll("textarea");
-  let l = inputs.length;
-  for(let i = 0; i < l; i++) {
-    const input = inputs[i];
-    switch (input.type) {
-      case "radio":
-        if(errors[input.name]) {
-          setInvalidInput(input);
-        }
-          break;
-      case "checkbox":
-        if(errors[input.name]) {
-          setInvalidInput(input);
-        }
-          break;
-      case "file":
-        if(errors[input.name]) {
-          setInvalidInput(input);
-        }
-          break;
-      default:
-        if(errors[input.name]) {
-          setInvalidInput(input);
-          errorMessageInputCreate(input, errors[input.name]);
-        }
-          break;
+  
+  function mailCheck (email) {
+    return email.match(/^[0-9a-z-\.]+\@[0-9a-z-]{2,}\.[a-z]{2,}$/i);
+  }
+  
+  function errorMessageInputCreate(input, text) {
+    let message = document.createElement("div");
+    message.classList.add("invalid-feedback");
+    message.innerText = text;
+  
+    input.insertAdjacentElement("afterend", message);
+    input.addEventListener("input", function hendlerInput(event) {
+      message.remove();
+      input.removeEventListener("input", hendlerInput);
+    })
+  }
+  
+  function setInvalidInput (input) {
+    input.classList.add("form-input_notvalid");
+    input.addEventListener("input", function hendlerInput(event) {
+      input.classList.remove("form-input_notvalid");
+      input.removeEventListener("input", hendlerInput);
+    })
+  }
+  
+  function setFormErrors (form, errors) {
+    const inputs = form.querySelectorAll("input");
+    const textares = form.querySelectorAll("textarea");
+    let l = inputs.length;
+    for(let i = 0; i < l; i++) {
+      const input = inputs[i];
+      switch (input.type) {
+        case "radio":
+          if(errors[input.name]) {
+            setInvalidInput(input);
+          }
+            break;
+        case "checkbox":
+          if(errors[input.name]) {
+            setInvalidInput(input);
+          }
+            break;
+        case "file":
+          if(errors[input.name]) {
+            setInvalidInput(input);
+          }
+            break;
+        default:
+          if(errors[input.name]) {
+            setInvalidInput(input);
+            errorMessageInputCreate(input, errors[input.name]);
+          }
+            break;
+      }
     }
-  }
-}
-
-let formSignUp = document.forms["sign-up"];
-
-
-(function () {  
-  formSignUp.addEventListener("submit", function(event) {
+  } 
+  
+  (function () {
+    let formSignUp = document.forms["sign-up"];  
+    formSignUp.addEventListener("submit", function(event) {
     event.preventDefault();
     const form = event.target;
     const values = getValuesForm(form);
@@ -243,87 +223,9 @@ let formSignUp = document.forms["sign-up"];
       }  
     setFormErrors(form, errors);
     });     
-}) ();
-
-(function openPopupSignUp () {
-  let buttonOpenSignUp = document.querySelector(".button-sign_js");
-  let buttonCloseSignUp = formSignUp.querySelector(".form-sign__button-close_js");
-  let inputSignUpFocus = formSignUp.querySelector("input[name=email]");
-  let popupSignUp = document.querySelector(".sign-popup_js");
-  console.log(buttonCloseSignUp);
-  
-  
-  buttonOpenSignUp.addEventListener("click", function () {
-    popupSignUp.classList.remove("sign-popup_none");
-    inputSignUpFocus.focus();    
-  });
-  
-  buttonCloseSignUp.addEventListener("click", function() {
-    popupSignUp.classList.add("sign-popup_none");
-    buttonOpenSignUp.focus();
-  });
-  
-  window.addEventListener("keydown", function (event) {
-    if (event.code == "Escape" && !popupSignUp.classList.contains("sign-popup_none")) {
-      popupSignUp.classList.add("sign-popup_none");
-      buttonOpenSignUp.focus();        
-    }
-  });
-})();
-
-// let formMessage = document.forms["message"];
-
-// (function openPopupMessage () {
-//   let buttonOpenMessage = document.querySelector(".button-message_js");
-//   let buttonCloseMessage = formMessage.querySelector(".form-message__button-close_js");
-//   let inputMessageFocus = formMessage.querySelector("input[name=name]");
-//   let popupMessage = document.querySelector(".message-popup")
-  
-  
-//   buttonOpenMessage.addEventListener("click", function () {
-//     popupMessage.classList.remove("message-popup_none");
-//     inputMessageFocus.focus();
-//   });
-  
-//   buttonCloseMessage.addEventListener("click", function() {
-//     popupMessage.classList.add("message-popup_none");
-//     buttonOpenMessage.focus();
-//   });
-  
-//   window.addEventListener("keydown", function (event) {
-//     if (event.code == "Escape" && !popupMessage.classList.contains("message-popup_none")) {
-//       popupMessage.classList.add("message-popup_none");
-//       buttonOpenMessage.focus();        
-//     }
-//   });
-// })();
+  }) ();
 
 
 
 
-// let formLogin = document.forms["login"];
 
-// (function openPopupLogin () {
-//   let buttonOpenLogin = document.querySelector(".button-login_js");
-//   let buttonCloseLogin = formLogin.querySelector(".form-login__button-close_js");
-//   let inputLoginFocus = formLogin.querySelector("input[name=email]");
-//   let popupLogin = document.querySelector(".login-popup")
-  
-  
-//   buttonOpenLogin.addEventListener("click", function () {
-//     popupLogin.classList.remove("login-popup_none");
-//     inputLoginFocus.focus();
-//   });
-  
-//   buttonCloseLogin.addEventListener("click", function() {
-//     popupLogin.classList.add("login-popup_none");
-//     buttonOpenLogin.focus();
-//   });
-  
-//   window.addEventListener("keydown", function (event) {
-//     if (event.code == "Escape" && !popupLogin.classList.contains("login-popup_none")) {
-//       popupLogin.classList.add("login-popup_none");
-//       buttonOpenLogin.focus();        
-//     }
-//   });
-// })();
--- src/static/images/index.js
+++ src/static/images/index.js
@@ -1,7 +1,5 @@
-alert("sada");
-let formMessage = document.forms["message"];
-
 (function openPopupMessage () {
+  let formMessage = document.forms["message"];
   let buttonOpenMessage = document.querySelector(".button-message_js");
   let buttonCloseMessage = formMessage.querySelector(".form-message__button-close_js");
   let inputMessageFocus = formMessage.querySelector("input[name=name]");
@@ -26,12 +24,8 @@ let formMessage = document.forms["message"];
   });
 })();
 
-
-
-
-let formLogin = document.forms["login"];
-
 (function openPopupLogin () {
+  let formLogin = document.forms["login"];
   let buttonOpenLogin = document.querySelector(".button-login_js");
   let buttonCloseLogin = formLogin.querySelector(".form-login__button-close_js");
   let inputLoginFocus = formLogin.querySelector("input[name=email]");
@@ -52,6 +46,49 @@ let formLogin = document.forms["login"];
     if (event.code == "Escape" && !popupLogin.classList.contains("login-popup_none")) {
       popupLogin.classList.add("login-popup_none");
       buttonOpenLogin.focus();        
+    }
+  });
+})();
+
+(function openPopupSignUp () {
+  let formSignUp = document.forms["sign-up"];
+  let buttonOpenSignUp = document.querySelector(".button-sign_js");
+  let buttonCloseSignUp = formSignUp.querySelector(".form-sign__button-close_js");
+  let inputSignUpFocus = formSignUp.querySelector("input[name=email]");
+  let popupSignUp = document.querySelector(".sign-popup_js");
+  
+  
+  buttonOpenSignUp.addEventListener("click", function () {
+    popupSignUp.classList.remove("sign-popup_none");
+    inputSignUpFocus.focus();    
+  });
+  
+  buttonCloseSignUp.addEventListener("click", function() {
+    popupSignUp.classList.add("sign-popup_none");
+    buttonOpenSignUp.focus();
+  });
+  
+  window.addEventListener("keydown", function (event) {
+    if (event.code == "Escape" && !popupSignUp.classList.contains("sign-popup_none")) {
+      popupSignUp.classList.add("sign-popup_none");
+      buttonOpenSignUp.focus();        
+    }
+  });
+})();
+
+(function scrollTop () {
+  let buttonTop = document.querySelector(".button-arrow_js");  
+
+  window.addEventListener("scroll", function(event) {
+    if (window.pageYOffset > 1500) {
+      buttonTop.classList.remove("hidden");
+      buttonTop.addEventListener("click", function handler(event) {
+        buttonTop.classList.add("hidden");
+        window.scrollTo({top: 0, behavior: "auto"});
+      })
+    }
+    if (window.pageYOffset < 1500) {
+      buttonTop.classList.add("hidden");
     }
   });
 })();
